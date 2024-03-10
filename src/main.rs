e68ed3b9e7653df17d545f06c978497c4c2edff0
lucapl <lukasz.andryszewski@outlook.com>
2024-03-10 01:09:52 +0100

    Random permutation generator

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
 
     let max = 10;
 
-    for i in 0..1000{
+    for _i in 0..3{
         let x = rand.random(max);
         let y = rand.different_random(x,max);
         println!("Random number {}, different random {}",x,y);
@@ -16,4 +16,6 @@ fn main() {
             println!("!!! RANDOM GENERATOR DOES NOT WORK !!!")
         }
     }
+    let random_perm = *rand.randomPermutation(10);
+    println!("Random permutation {:?}",random_perm);
 }
--- src/utils/randomizer.rs
+++ src/utils/randomizer.rs
@@ -1,4 +1,6 @@
 use rand::prelude::*;
+use std::vec::Vec;
+use std::boxed::Box;
 
 pub struct Randomizer{
     rng: ThreadRng,
@@ -17,6 +19,20 @@ impl Randomizer{
 
     pub fn different_random(&mut self,x:u32,_max:u32)->u32{
         return (self.random(_max-1)+1+x)%_max
+    }
+
+    pub fn shuffle(&mut self, v: &mut Vec<i32>){
+        let n = v.len() as u32;
+        for i in 0..n{
+            let r = self.random(n-i)+i;
+            v.swap(i as usize,r as usize);
+        }
+    }
+
+    pub fn randomPermutation(&mut self,length:i32)->Box<Vec<i32>>{
+        let mut permutation:Vec<i32> = (0..length).collect();
+        self.shuffle(&mut permutation);
+        Box::new(permutation)
     }
 
 }
